f2887ecb53ccc6cda85d8aa89dadc2c8a49b3602
thiago-scherrer <eu@thiago.info>
2021-04-30 08:57:57 -0300

    Z_PROBE_SPEED_SLOW rename

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -994,7 +994,7 @@
 #define Z_PROBE_FEEDRATE_FAST HOMING_FEEDRATE_Z
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
+#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
 
 /**
  * Multiple Probing
